932b196cf7d3198c6f6b441d0896d599f70a4beb
Rafael Cunha <51427048+rocunha09@users.noreply.github.com>
2022-04-10 20:08:25 -0300

    crud para instituições iniciado

---
+++ Controllers/InstituicaoController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Projeto_IES_ASPNET_CORE_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto_IES_ASPNET_CORE_MVC.Controllers
+{
+    public class InstituicaoController : Controller
+    {
+        private static IList<Instituicao> Instituicoes = new List<Instituicao>()
+        {
+            new Instituicao()
+            {
+                InstituicaoID = 1,
+                Nome = "Estacio",
+                Endereco = "Rio de Janeiro"
+            },
+            new Instituicao()
+            {
+                InstituicaoID = 2,
+                Nome = "Unisuam",
+                Endereco = "Rio de Janeiro"
+            },
+            new Instituicao()
+            {
+                InstituicaoID = 3,
+                Nome = "Anhembi Morumbi",
+                Endereco = "São Paulo"
+            }
+        };
+        public IActionResult Index()
+        {
+            return View(Instituicoes);
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Projeto_IES_ASPNET_CORE_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
